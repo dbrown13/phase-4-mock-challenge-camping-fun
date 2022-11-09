b382ae4dd806f62a367a1cf9d19c69ebd433c35f
Deryn Brown <Deryn.2017@gmail.com>
2022-11-09 10:28:24 -0600

    commit tree 4751cdf263ee5d5c3ce41ddfd2769c7163e13c2b

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+
+  private 
+
+  def render_not_found_response(e)
+    render json: { error: "#{e.model} not found" }, status: :not_found
+  end
+
+  def record_invalid(e)
+    render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
 
 end
